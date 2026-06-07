888ba133d111c1cce603b195209dc1dd29b11d7c
Tagwa <taqwa00osman@gmail.com>
2026-06-07 15:45:20 +0300

    StudentProfile component

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import Hello from "./Hello";
 import Card from "./First";
+import StudentProfile from "./Second"
 
 function App() {
   return (
     <div>
       <Card />
+      <StudentProfile />
       {/* write your compoents here */}
     </div>
   );
