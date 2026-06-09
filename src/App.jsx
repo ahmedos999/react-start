fbcd288c987ae277e7bb716a60a7445ffabf7c4e
TaqwaOsman <taqwa00osman@gmail.com>
2026-06-09 22:44:49 +0300

    tagwa-task2

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 import Hello from "./Hello";
 import Card from "./First";
 import StudentProfile from "./Second"
-
+import Counter from "./Counter";
 function App() {
   return (
     <div>
       <Card />
       <StudentProfile />
+      <Counter />
       {/* write your compoents here */}
     </div>
   );
